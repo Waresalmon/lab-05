229909be416b61e618f774aa48ba88c6316f628a
Waresalmon <86568693+Waresalmon@users.noreply.github.com>
2026-02-05 14:51:50 -0700

    firebase is set up

--- ListyCity/app/build.gradle.kts
+++ ListyCity/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
 
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,4 +43,6 @@ dependencies {
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 
-}
+    //Import Firebase
+    implementation(platform("com.google.firebase:firebase-bom:34.8.0"))
+    implementation("com.google.firebase:firebase-analytics")}
